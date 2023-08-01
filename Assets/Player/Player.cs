1df4b43ff6f8635acf6cafec356f18c13cef740a
housecats2020 <1228187419@qq.com>
2023-08-01 20:56:45 +0800

    耐力变化 弹反无敌

--- Assets/Player/Player.cs
+++ Assets/Player/Player.cs
@@ -2,11 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.InputSystem;
 
 [RequireComponent(typeof(Rigidbody2D))]
 public class Player : MonoBehaviour
 {
+    public event UnityAction<float,float> onStaminaChanged;
+
     new Rigidbody2D rigidbody;
     private Vector2 moveVector2 = Vector2.zero;
     private bool playerIsBounceBack = false;
@@ -16,8 +19,12 @@ public class Player : MonoBehaviour
     [SerializeField,Tooltip("玩家加速时间")] private float playerAccelerationTime;
     [SerializeField,Tooltip("玩家减速时间")] private float playerDecelerationTime;
     [SerializeField, Tooltip("玩家耐力值")] private float playerStamina;
+    [SerializeField, Tooltip("玩家最大耐力值")] private float playerMaxStamina; 
+    [SerializeField, Tooltip("玩家耐力值恢复速度")] private float playerStaminaRecoverSpeed;
     [SerializeField, Tooltip("玩家弹反消耗")] private float playerBounceBackCost;
     [SerializeField, Tooltip("玩家弹反CD")] private float playerBounceBackCD;
+
+    public bool playerIsInvicible = false;
     //[SerializeField, Tooltip("玩家难绷值")] private float playerTough = 0;
     //[SerializeField, Tooltip("最大难绷值")] private float playerMaxTough = 100f;
     // Start is called before the first frame update
@@ -44,6 +51,7 @@ public class Player : MonoBehaviour
     {
         rigidbody.gravityScale = 0f;
         input.EnablePlayerInputs();
+        playerStamina = playerMaxStamina;
     }
 
     
@@ -70,10 +78,19 @@ public class Player : MonoBehaviour
     {
         if(playerBounceBackCost <= playerStamina && !playerIsBounceBack)
         {
-            
             StopCoroutine(nameof(BounceBackCoroutine));
             playerStamina -= playerBounceBackCost;
+            onStaminaChanged.Invoke(playerStamina, playerMaxStamina);
             StartCoroutine(nameof(BounceBackCoroutine));
+        }
+    }
+
+    void StaminaRecover()
+    {
+        StopCoroutine(nameof(StaminaRecoverCoroutine));
+        if (playerStamina < playerMaxStamina)
+        {
+            StartCoroutine(nameof(StaminaRecoverCoroutine));
         }
     }
 
@@ -94,12 +111,29 @@ public class Player : MonoBehaviour
         float t = 0f;
         Debug.Log(playerStamina);
         playerIsBounceBack = true;
+        playerIsInvicible = true;
         while (t < playerBounceBackCD)
         {
             t += Time.deltaTime;
             yield return null;
         }
         playerIsBounceBack = false;
+        playerIsInvicible = false;
+        StaminaRecover();
     }
 
+    //耐力恢复
+    IEnumerator StaminaRecoverCoroutine()
+    {
+        while(playerStamina <= playerMaxStamina)
+        {
+            if (playerIsBounceBack)
+            {
+                StopCoroutine(nameof(StaminaRecoverCoroutine));
+            }
+            playerStamina += Time.deltaTime* playerStaminaRecoverSpeed;
+            onStaminaChanged.Invoke(playerStamina,playerMaxStamina);
+            yield return null;
+        }
+    }
 }
--- Assets/Player/PlayerHealth.cs
+++ Assets/Player/PlayerHealth.cs
@@ -1,15 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
+    [SerializeField, Tooltip("玩家")] public Player player;
     [SerializeField, Tooltip("玩家难绷值")] public float Tough = 0;
     [SerializeField, Tooltip("最大难绷值")] public float MaxTough = 100f;
     public Image NanBeng;
     public Image NaiLi;
-   
+
+    private void Awake()
+    {
+        player = GetComponent<Player>();
+        player.onStaminaChanged += StaminaChanged;//耐力事件绑定
+    }
+
     private void Start()
     {
         Amount();
@@ -21,8 +29,11 @@ public class PlayerHealth : MonoBehaviour
            
     }
     public void Harm(float damage) {
-        Tough += damage;
-        Amount();
+        if(!player.playerIsInvicible)//无敌判定
+        {
+            Tough += damage;
+            Amount();
+        }
     }
 
     void Amount() {
@@ -36,4 +47,16 @@ public class PlayerHealth : MonoBehaviour
             Harm(collision.GetComponentInParent<EnemyAction>().damage);
         }
     }
+
+    //耐力变化回调
+    public void StaminaChanged(float playerStamina, float playerMaxStamina)
+    {
+        if(playerStamina > playerMaxStamina)
+        {
+            playerStamina = playerMaxStamina;
+        }
+        NaiLi.fillAmount = playerStamina / playerMaxStamina;
+    }
+
+
 }
